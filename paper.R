ff071b2e9b074b370f861d6c982953bb62510d57
TSR2 <th0928367718@hotmail.com>
2015-09-30 01:15:15 +0800

    1

---
+++ paper.R
@@ -0,0 +1,95 @@
+
+#####################隨機產生直方圖資料
+a=list()
+total=list()
+for (j in 1:4){
+  for (i in 1:30){
+    his <- hist(iris[sample(x=1:150,50),j])
+    a[[i]] <- his
+  }
+  total[[j]] <- a
+}
+######################################相同直方圖
+a=list()
+total=list()
+for (j in 1:4){
+  for (i in 1:30){
+    his <- hist(iris[,j])
+    a[[i]] <- his
+  }
+  total[[j]] <- a
+}
+
+
+
+##################畫直方圖
+par(mfrow=c(6,4),mai=c(0,0,0,0))
+for (j in 1:4){
+  for (i in 1:30){
+    plot(total[[j]][[i]],main = "",xlab = "",ylab = "")
+  }
+}
+his
+length(total[[1]][[1]][[1]])
+
+#########################算平均
+
+mu=c()
+for (j in 1:4){
+  sum=0
+  for (i in 1:30){
+    for (b in 1:(length(total[[j]][[i]][[1]])-1)){
+      a=total[[j]][[i]][[2]][b]*(total[[j]][[i]][[1]][b+1]-total[[j]][[i]][[1]][b])
+    sum=a+sum
+    }
+  }
+  mu=c(mu,sum/(2*i))
+}
+mu
+########################算平均  向量
+
+mu=c()
+for (j in 1:4){
+  sum=0
+  for (i in 1:30){
+    b=length(total[[j]][[i]][[1]])-1
+    a=total[[j]][[i]][[2]]*(total[[j]][[i]][[1]][2:(b+1)]-total[[j]][[i]][[1]][1:b])
+    sum=sum+sum(a)
+  }
+  mu=c(mu,sum/(2*i))
+}
+mu
+
+
+##################################test
+cal=function(x){
+  b=length(x[[1]])-1
+  a=x[[2]]*(x[[1]][2:(b+1)]-x[[1]][1:b])
+  sum(a)
+}
+n=length(total[[1]])
+mu=c()
+for (j in 1:4){
+  m=sum(sapply(total[[j]],cal))/(2*n)
+  mu=c(mu,m)
+}
+mu
+
+##################################var parallel
+
+cal=function(x){
+  b=length(x[[1]])-1
+  #parta=x[[2]]*(x[[1]][2:(b+1)]-x[[1]][1:b])^2
+  parta=x[[2]]*(x[[1]][1:b]^2+x[[1]][1:b]*x[[1]][2:(b+1)]+x[[1]][2:(b+1)]^2)
+  sum(parta)
+}
+n=length(total[[1]])
+EX2=c()
+for (j in 1:4){
+  m=sum(sapply(total[[j]],cal))/(3*n)
+  EX2=c(EX2,m)
+}
+EX2
+var=EX2-mu^2
+var
+
