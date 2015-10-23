a99b8cd970f65e3d925425e8e79097e9e50c88f2
TSR2 <th0928367718@hotmail.com>
2015-10-24 01:02:13 +0800

    1

--- data table practice.R
+++ data table practice.R
@@ -6,7 +6,7 @@ library(data.table)
 write.csv(iris,file="c:/Users/tsr/Desktop/iris.csv")
 x=iris
 names(x)=paste('v',1:5,sep="")
-x=tbl(x)
+x=setDT(x)
 x[x$v1>7,]
 x %$% x[v1>7,]
 cc=x %>% group_by(v5) %>% do(model=lm(v1~v2,data=.))
--- run string.R
+++ run string.R
@@ -4,3 +4,11 @@ eval(parse(text="hist(iris[,2])"))
 
 
 hist(eval(parse(text="iris[,3]")))
+
+#######################
+
+x=iris
+na=names(x[,1:3])
+kk=paste(na,sep="",collapse = "+")
+kk=paste("Petal.Width",kk,sep="~")
+lm(as.formula(kk),data=x)
