1ca1ed9b1834deeed946b6e583721e493eb9760c
TSR2 <th0928367718@hotmail.com>
2015-10-28 20:35:23 +0800

    1

--- 345345.R
+++ 345345.R
@@ -1,2 +1,21 @@
-###¤Ñ¤Ñ
-1212
+f=function(x,y)x+y
+f(1:9,2:11)
+
+
+
+f=function(x,y) x^2-y
+sq=function(y){
+  v=1;s=100
+  while (all(s>(10^-3))){
+    v=v-f(v,y)/(2*v)
+    s=f(v,y)
+  }
+  v
+}
+sq(c(5,7))
+
+par(col=4,bg=3)
+plot(1:10)
+sel=identify(1:10,labels = 1:3)
+
+
