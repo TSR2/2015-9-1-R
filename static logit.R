3f27640c1f85a656b23c552d3e0e7c1d41c3b0c4
TSR2 <th0928367718@hotmail.com>
2015-11-09 21:05:19 +0800

    1

--- static logit.R
+++ static logit.R
@@ -1,3 +1,5 @@
+library(foreign)
+
 x=read.table("c:/Users/tsr/Desktop/上課用檔案/統計諮詢/CHD.csv",sep=",",head=T)
 head(x)
 mo=glm(chd~age,family = "binomial",data=x)
@@ -5,9 +7,14 @@ mo
 names(mo)
 exp(mo[[1]])
 names1=paste("ms",1:7,sep="")
-for (i in names1){
-  x$
-}
 b=x
 
 b$as.formula("ms1")=2
+
+
+x=read.spss("c:/Users/tsr/Desktop/上課用檔案/統計諮詢/FEV.sav",to.data.frame=T)
+head(x)
+xx=x
+xx$sex=as.factor(xx$sex)
+mo=glm(fev~sex+age+hgt,data=xx)
+summary(mo)
